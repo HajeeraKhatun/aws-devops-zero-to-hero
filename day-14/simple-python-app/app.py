61e215458cee77e6bc79f60b08a671c7c9e3f919
HajeeraKhatun <47415269+HajeeraKhatun@users.noreply.github.com>
2024-09-07 17:34:18 +0530

    Update app.py added the web page content

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Application launch page "\n" Congratulations on hosting your first containerized application! "\n" Application programming language "\n" Python "\n" Cloud provider "\n" AWS cloud "\n" Containerization tool "\n" Docker'
 
 if __name__ == '__main__':
     app.run()
